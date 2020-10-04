32935d8bde9822e175924ad8685007be688163e4
Ariel Ibarra <arielibarra39@gmail.com>
2020-10-04 05:49:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,9 +11,9 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    students_array.each do |s|
-      
+    students_array.each do |students_hash|
     Student.new(students_hash)
+  end 
   end
 
   def add_student_attributes(attributes_hash)
